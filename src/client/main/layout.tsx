daf99284f5a2aa2606d685f11a40a0ad1c20851e
vnues <1518190293@qq.com>
2021-05-02 01:24:30 +0800

    feat: update base

--- src/client/main/layout.tsx
+++ src/client/main/layout.tsx
@@ -6,7 +6,6 @@ import { hot } from 'react-hot-loader/root'
 // @ts-ignore
 import css from './layout.less'
 
-// @ts-ignore
 import withStyles from 'isomorphic-style-loader/withStyles'
 
 class Index extends React.PureComponent<any, any> {

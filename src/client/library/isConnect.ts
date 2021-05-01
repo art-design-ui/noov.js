b3ad5ea6d4d7fef4df3ad120fb2277effc024ef6
vnues <1518190293@qq.com>
2021-05-02 01:01:27 +0800

    feat: update base

--- src/client/library/isConnect.ts
+++ src/client/library/isConnect.ts
@@ -2,7 +2,7 @@ import Container from '@/components/container'
 import { connect } from 'react-redux'
 // @ts-ignore
 import withStyles from 'isomorphic-style-loader/withStyles'
-
+console.log('1')
 export default (
   { css, mapStateToProps, mapDispatchToProps }: any,
   ActiveComponet: any

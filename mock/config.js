4693f28f2626e57d01f0075e1e11676d365e5591
vnues <1518190293@qq.com>
2021-05-07 11:39:35 +0800

    feat: update mock

--- mock/config.js
+++ mock/config.js
@@ -4,16 +4,15 @@ module.exports = {
   directory: './',
   cors: true,
   watch: true,
-  proxy: 'www.pandateacher.com',
+  proxy: '',
   to: '127.0.0.1:8080',
   wechatConfig: {
-    appId: 'wxe4e7b9b1082e1394',
-    secret: '2f0b82dde418cdf57ee9dfa6ef021f5f',
+    appId: '',
+    secret: '',
     // debug: true,
     // timestamp: '',
     // nonceStr: '',
-    url: 'http://www.pandateacher.com/test.html',
-    jsApiList: ['chooseImage', 'previewImage']
+    url: '',
+    jsApiList: []
   }
 };
- 
